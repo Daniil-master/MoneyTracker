1da70dce6b50ef7cb77c741630fd03fd9ffb5c8c
Daniil K <daniilkamornicki@gmail.com>
2019-04-01 21:02:19 +0300

    HomeWork2 system Rub

--- app/src/main/java/online/daniilk/moneytracker/AddItemActivity.java
+++ app/src/main/java/online/daniilk/moneytracker/AddItemActivity.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -30,44 +31,30 @@ public class AddItemActivity extends AppCompatActivity {
 
         addBtn.setEnabled(false);
 
-        name.addTextChangedListener(new TextWatcher() {
+        TextWatcher txtWatcher =  new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                addBtn.setEnabled(!TextUtils.isEmpty(name.getText()) && !TextUtils.isEmpty(price.getText()));
-
-            }
-        });
-
-        price.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-//                if(!TextUtils.isEmpty(price.getText())){
-                if(editable.toString() != null){
-                    price.setText(price.getText().toString() + getString(R.string.rub));
+                if(price.getText() == charSequence){
+                    Toast.makeText(AddItemActivity.this, "Price ID", Toast.LENGTH_SHORT).show();
+                   // if(price.getText().equals(null))
+                        price.setText(getString(R.string.rub));
                 }
-                addBtn.setEnabled(!TextUtils.isEmpty(name.getText()) && !TextUtils.isEmpty(price.getText()));
-                //addBtn.setEnabled(!TextUtils.isEmpty(editable));
             }
-        });
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                addBtn.setEnabled(!TextUtils.isEmpty(name.getText()) && !TextUtils.isEmpty(price.getText()));
+
+            }
+        };
+
+        name.addTextChangedListener(txtWatcher);
+        price.addTextChangedListener(txtWatcher);
 
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
